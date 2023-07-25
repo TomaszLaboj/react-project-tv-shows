36d2f66da105e780fc133fd73d88098469e619ab
TomaszLaboj <tomasz.laboj@gmail.com>
2023-07-25 11:23:27 +0100

    added function format season and episode

--- src/components/episodesComponents.tsx
+++ src/components/episodesComponents.tsx
@@ -22,6 +22,24 @@ interface EpisodeViewProp {
   episode: IEpisode;
 }
 
+function formatEpisodeCode(season:number,episode:number):string{
+  let seasonCode: string ;
+  let episodeCode: string;
+  
+  if(season<10){
+    seasonCode = "S0"+season
+  }else{
+    seasonCode = "S"+season
+  }
+  if(episode<10){
+    episodeCode = seasonCode+"E0"+episode
+  }else{
+    episodeCode = seasonCode+"E"+episode
+  }
+    return episodeCode;
+  }
+  
+
 export default function DisplayEpisodes(props: EpisodeViewProp) {
   return (
     <div className="episode">
